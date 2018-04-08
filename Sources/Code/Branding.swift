91acd9d27c48fa4557117bb7ac68c9641028a685
Vitalij Saponenko <vitalij.saponenko@jamitlabs.com>
2018-04-08 21:39:55 +0200

    Setup navigation bar global appearance & add custom font

--- Sources/Code/Branding.swift
+++ Sources/Code/Branding.swift
@@ -11,9 +11,26 @@ import UIKit
 class Branding {
     // MARK: - Stored Type Properties
     static let shared = Branding()
+    private let customFont = UIFont(name: "Rubik-Medium", size: 16)
 
     // MARK: - Instance Methods
     func setupGlobalAppearance() {
         UIView.appearance().tintColor = UIColor(named: .accent)
+    }
+
+    private func setupNavigationBarAppearance() {
+        let navBarAppearance = UINavigationBar.appearance()
+
+        let titleTextAttributes = [
+            NSAttributedStringKey.font: customFont?.withSize(26) ?? UIFont.systemFont(ofSize: 26),
+            NSAttributedStringKey.foregroundColor: Color(named: .primary)
+        ]
+
+        navBarAppearance.titleTextAttributes = titleTextAttributes
+        navBarAppearance.backItem?.backBarButtonItem?.title = ""
+        navBarAppearance.shadowImage = UIImage()
+        navBarAppearance.tintColor = .white
+        navBarAppearance.barTintColor = UIColor(named: .secondary)
+        navBarAppearance.isTranslucent = false
     }
 }
