03d04b3d9d6ebd737382c6c3b4c7c2b3241ae794
Vitalij Saponenko <vitalij.saponenko@jamitlabs.com>
2018-04-21 16:40:53 +0200

    Setup AppDelegate settings

--- Sources/Code/AppDelegate.swift
+++ Sources/Code/AppDelegate.swift
@@ -6,17 +6,32 @@
 //  Copyright © 2017 Jamit Labs GmbH. All rights reserved.
 //
 
+import Imperio
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Stored Type Properties
+    static var shared: AppDelegate?
+
     // MARK: - Stored Instance Properties
-    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
+
+    // MARK: - Computed Instance Properties
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        AppDelegate.shared = self
+        window?.makeKeyAndVisible()
+
         // setup global stuff
         Logger.shared.setup()
         Branding.shared.setupGlobalAppearance()
+
+        // TODO - check here if onboarding needs to be dislpayed -> set onboarding coordinator as AppCoordinator
+        appCoordinator = MenuCoordinator.shared
+        //appCoordinator = LoginRegisterCoordinator.shared
+        appCoordinator?.start()
 
         return true
     }
--- Sources/Constants/Storyboards.swift
+++ Sources/Constants/Storyboards.swift
@@ -61,7 +61,7 @@ internal enum StoryboardScene {
   internal enum Main: StoryboardType {
     internal static let storyboardName = "Main"
 
-    internal static let initialScene = InitialSceneType<ViewController>(storyboard: Main.self)
+    internal static let initialScene = InitialSceneType<PageViewController>(storyboard: Main.self)
   }
 }
 
